259077086423262966d450b5a1c03fa58ef79cfb
unknown <kronossolucionestic@gmail.com>
2020-08-28 16:55:28 -0500

    Inicial

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ejemploconsume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String url = "http://localhost/apiRESTPHP/restApiPHP/api/handler.php";
+            var json = new WebClient().DownloadString(url);
+            dynamic m = JsonConvert.DeserializeObject(json);
+            //Console.WriteLine(json);
+            foreach(var i in m){
+                Console.WriteLine(i.nombre);
+                Console.Read();
+            }
+        }
+    }
+}
